88fc0a619477a238faa14df6e9ca4100645092b6
yahyaoui98 <49151336+yahyaoui98@users.noreply.github.com>
2020-02-27 23:59:00 +0100

    Carte Interactive

---
+++ js/recherche.js
@@ -0,0 +1,18 @@
+/*$(function () {
+    $('#categoryFilter').onkeyup(function () {
+        var val = $(this).val();
+        var regexp = '\\b(.*)(a)(.*)\\b';
+        $()
+    })
+})
+
+
+
+$(function () {
+    "use strict";
+    // Click on li item will change input text value
+    $('li').click(function () {
+        $('#fruit-selector').val($(this).html());
+    });*/
+
+    // Type some text into input will hide some fruit !
