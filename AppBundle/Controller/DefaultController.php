1fd66f20f7d3327031b3076b26d1256b79d3bde2
yahyaoui98 <49151336+yahyaoui98@users.noreply.github.com>
2020-02-28 00:31:59 +0100

    Message Bundle mnadhma

---
+++ AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+
+        $authChecker = $this->container->get('security.authorization_checker');
+        if($authChecker->isGranted('ROLE_ADMIN'))
+        {
+            return $this->redirectToRoute('addF');
+        }
+        else if ($authChecker->isGranted('ROLE_USER'))
+        {
+            return $this->redirectToRoute('affiche_front');
+        }
+        return $this->render('baseok.html.twig');
+    }
+}
